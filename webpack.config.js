f6e158431db3849a0662d2b7410ab9fe4725d907
limozaza <zakboite@gmail.com>
2017-09-02 21:25:18 +0200

    Add webpack fisrt step

---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+const path = require('path');
+module.exports = {
+    entry: './assets/js/app.js',
+    watch: true,
+     output: {
+         path: path.resolve('./dist'),
+         filename: 'bundle.js'
+     }
+}
